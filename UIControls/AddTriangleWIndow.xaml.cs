3e66733382724ab7a1420fd700a14cb9793973ae
Rostyslav <st1tchqwerty@gmail.com>
2024-05-09 10:20:48 +0200

    Add triangle

--- UIControls/AddTriangleWIndow.xaml.cs
+++ UIControls/AddTriangleWIndow.xaml.cs
@@ -11,14 +11,25 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TempoEngine.Engine;
+using Point = System.Windows.Point;
 
 namespace TempoEngine.UIControls {
     /// <summary>
     /// Interaction logic for AddTriangleWIndow.xaml
     /// </summary>
     public partial class AddTriangleWIndow : Window {
+        GrainTriangle _newTriangle;
         public AddTriangleWIndow() {
             InitializeComponent();
+            _newTriangle = new GrainTriangle("New Triangle", new Point(0, 0), new Point(0, 0), new Point(0, 0));
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            // Set the default values for the textboxes
+            tbName.Text = _newTriangle.Name;
+
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e) {
@@ -28,6 +39,10 @@ namespace TempoEngine.UIControls {
         private void CancelButton_Click(object sender, RoutedEventArgs e) {
             // Close the window
             Close();
+        }
+
+        private void Button_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if(e.Key != Key.Enter) return;
         }
     }
 }
