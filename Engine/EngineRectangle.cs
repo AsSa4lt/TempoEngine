65292703084a4a69309d70571ea43b594f998d02
AsSa4lt <st1tchqwerty@gmail.com>
2024-07-17 21:49:57 +0200

    EngineRectangle

---
+++ Engine/EngineRectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using TempoEngine.UIControls;
+
+namespace TempoEngine.Engine {
+    /**
+     * \class EngineRectangle
+     * \brief Object of the engine that represents rectangle
+     * 
+     * 
+     * Manages itself and makes it easier to calculate transfers
+     * \see EngineObject
+     * \see CanvasManager
+     */
+    internal class EngineRectangle : EngineObject {
+        public EngineRectangle(string name, int width, int height) : base(name) {
+            _size = new(width, height);
+        }
+
+        public override List<GrainSquare> GetExternalSquares() {
+            throw new NotImplementedException();
+        }
+
+        public override string GetJsonRepresentation() {
+            throw new NotImplementedException();
+        }
+
+        public override ObjectType GetObjectType() {
+            throw new NotImplementedException();
+        }
+
+        public override string GetObjectTypeString() {
+            throw new NotImplementedException();
+        }
+
+        public override void GetObjectVisibleArea(out Vector2 topLeft, out Vector2 bottomRight) {
+            throw new NotImplementedException();
+        }
+
+        public override List<Polygon> GetPolygons() {
+            throw new NotImplementedException();
+        }
+
+        public override List<GrainSquare> GetSquares() {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsIntersecting(EngineObject obj) {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsVisible(CanvasManager canvasManager) {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStartTemperature() {
+            throw new NotImplementedException();
+        }
+    }
+}
